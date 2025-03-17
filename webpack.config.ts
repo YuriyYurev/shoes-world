f8ad376ecf039bb30830ded7b6d3172bdb645709
YuriyYurev <iuriev-yuriy@mail.ru>
2025-03-17 10:39:52 +0700

    feat: dev-server && env

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,24 @@
 import path from "path";
 import { buildWebpackConfig } from "./config/buildWebpackConfig";
 import { Configuration } from "webpack";
+import { BuildEnv } from "config/types/config";
 
-const mode = "development";
-const isDev = mode === "development";
-const paths = {
-  entry: path.resolve(__dirname, "src", "index.ts"),
-  build: path.resolve(__dirname, "build"),
-  html: path.resolve(__dirname, "public", "index.html"),
+export default (env: BuildEnv) => {
+  const mode = env.mode || "development";
+  const PORT = env.port || 3000;
+  const isDev = mode === "development";
+
+  const paths = {
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const config: Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+  return config;
 };
-
-const config: Configuration = buildWebpackConfig({
-  mode,
-  paths,
-  isDev,
-});
-
-export default config;
