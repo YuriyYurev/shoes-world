81a22e0ee91b0110e0334420d37261ff4e2c4700
YuriyYurev <iuriev-yuriy@mail.ru>
2025-05-12 23:15:34 +0700

    feat: classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { Routes, Route, NavLink } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
 export default function App() {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button type="button" onClick={toggleTheme}>
         Toggle Theme
       </button>
